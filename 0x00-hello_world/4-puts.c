f1b03731e7cd8e7470faabd693b9e88c0ccb6ef8
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-08 19:34:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-/*
- * return 0
+/**
+ * main - main block
+ * return: 0
  */
 int main(void)
 {
-	puts("Progrmming is like building a multilingual puzzle\n")
-	return(0)
+puts("Progrmming is like building a multilingual puzzle\n");
+return (0);
 }

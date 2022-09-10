6ac75585c3052aa11b399d103f1851793d9f038b
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-09 20:34:59 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- program that prints all digit numbers from base 10 starting from 0
+ * you are not allowed to use any variable of type char
+ * you can only use the putchar funcion
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

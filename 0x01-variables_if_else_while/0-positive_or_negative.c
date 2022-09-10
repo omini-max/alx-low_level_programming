801859632cbd16068ba310f86d7572c2df77786b
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-09 18:26:13 -0700

    my commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* main - Entry point*/
-
-
-
-/* betty style doc for function main goes there */
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * if the number is greater than 0: is psitive, if the number is 0: is zero
+ * if the number is less than 0: is negative, followed by a new line
+ * Return: 0
+ */
 
 int main(void)
 
@@ -24,7 +25,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is 0\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }

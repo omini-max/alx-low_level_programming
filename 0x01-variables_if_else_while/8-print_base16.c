2998c3c717a7b242f7de18e17073eecc326587df
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-12 20:54:23 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	char alpha;
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	for (alpha = 'a'; alphat <= 'f'; alpha++)
+	putchar(alpha);
+	putchar('\n');
+	return (0);
+}

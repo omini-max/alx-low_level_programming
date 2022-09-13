4fb79f0d0424e1ce3eb822e7b963523776771191
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-12 20:24:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program that prints lower case letters in reverse then a new line.
+ * putchar can only be used twice
+ * all program must be in main
+ * Return: 0
+ */
+int main(void)
+{
+	int = a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	putchar(a);
+	putchar('\n');
+	return (0)
+}

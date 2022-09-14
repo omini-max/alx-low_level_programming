17152054914330efa4b70e28d6b2d8716ee9f47e
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-14 05:42:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - program that prints the alphabets, in lower case followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(print_alphabet());
+	_putchar('\n');
+	return (0);
+}

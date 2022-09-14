1cf144deb529670ea69ddc9308f458f805278ef9
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-14 11:00:30 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,15 @@
 
 int print_last_digit(int n)
 {
-	return (n >= 0 ? n : n * -1);
+	int nv;
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+	}
+	else
+	{
+		nv = n % 10;
+	}
+	_putchar(nv + '0');
+	return (nv);
 }

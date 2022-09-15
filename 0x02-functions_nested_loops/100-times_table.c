a64de2d3b219d9d4456aa9a0c690c8211a84943d
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-14 21:14:06 -0700

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,9 +18,7 @@ void print_times_table(int n)
 		{
 			rep = a * b;
 			if (b == 0)
-			{
 				_putchar('0' + rep);
-			}
 			else if (rep < 10)
 			{
 				_putchar(' ');
@@ -40,10 +38,8 @@ void print_times_table(int n)
 				_putchar('0' + rep % 10);
 			}
 			if (b < n)
-			{
 				_putchar(',');
 				_putchar(' ');
-			}
 		}
 		_putchar('\n');
 		a++;

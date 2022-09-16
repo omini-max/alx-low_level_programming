df741960ef20ca27a100fbda291ee9b380e0a4be
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-15 20:04:23 -0700

    commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,13 +13,20 @@
 int main(void)
 {
 	unsigned int long n = 612852475143, a = (int) sqrt(n);
+	int i;
 
 	while (1)
 	{
-		if (n % a == 0)
+		if ((n % a) == 0)
 		{
-			printf("%lu \n", a);
-			break;
+			for (i = (a - 1); i <= 2; i--)
+			{
+				if ((a % i) != 0)
+				{
+					printf("%lu \n", a);
+					break;
+				}
+			}
 		}
 		a--;
 	}

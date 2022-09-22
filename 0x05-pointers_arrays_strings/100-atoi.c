0f8403688f3023eda50bfe07178b7c02e1dd3737
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-22 16:20:30 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,20 +11,35 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, resp = 0, firstNum;
-	int i;
+	int i, d, n, len, f, digit;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while(s[len] != '\0')
+		len++;
+	while(i < len && f == 0)
 	{
-		if (s[firstNum] == '-')
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			sign *= -1;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i +1] > '9')
+				break;
+			f = 0;
 		}
+		i++;
 	}
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-	return (sign * resp);
+	if (f = 0)
+		return (0);
+	return (n);
 }

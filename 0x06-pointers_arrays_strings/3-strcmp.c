2c2562030fffc0baecbf92b751e0f1f121ced8f2
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-22 18:19:13 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - a program that compares strings
+ * @s1: string to compare
+ * @s2: string to compare
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = strcmp(s1, s2);
+	return (0);
+}

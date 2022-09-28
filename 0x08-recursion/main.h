45496736cb561f37f5f5fafc1e234f4375707b93
Chisom Okafor <chisomokafor575@gmail.com>
2022-09-28 15:14:01 -0700

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,7 @@ int _sqrt_recursion(int n);
 int square(int n, int val);
 int is_prime_number(int n);
 int check_prime(int n, int resp);
+int wildcmp(char *s1, char *s2);
+int last_index(char *s);
+int is_palindrome(char *s);
+int check(char *s, int start, int end, int pair);

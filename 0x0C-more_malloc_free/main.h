f79861af7c6b52412edc41aba7622ac74fe15d5f
Chisom Okafor <chisomokafor575@gmail.com>
2022-10-07 05:24:27 -0700

    advance 1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,4 +9,8 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void _memcpy(void *dest, void *src, size_t n);
+void _puts(char *str);
+void _puts(char *str);
+void print_int(unsigned long int n);
+int main(int argc, char const *argv[]);
 #endif
